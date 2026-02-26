49491ea23b33f3ff5278d90eb33c219a9684e804
Your Name <you@example.com>
2026-02-26 12:54:14 +0300

    добавила стайл в корень

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
-import './style.css'
+import '/style.css'
 createApp(App).mount('#app')
