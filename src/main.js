6998a18780a34e1c44adf6d91b7f9f4392bd7350
Your Name <you@example.com>
2026-02-27 11:32:42 +0300

    просто перенесла индекс2

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 createApp(App).mount('#app')
+console.log('Компоненты:', {
+  DayCounter: typeof DayCounter,
+  LoveMessage: typeof LoveMessage,
+  // ... остальные
+})
